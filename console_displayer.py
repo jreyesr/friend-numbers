0f90511825b5e28ac11dfdc22d6684cc4eb2539e
Reyes Ruiz <jreyesr@noreply.github.com>
2016-11-17 08:32:52 -0500

    Added console_displayer, to show tables on terminals. Still need to add the other way around

---
+++ console_displayer.py
@@ -0,0 +1,13 @@
+from string import ascii_uppercase
+
+
+class ConsoleDisplayer:
+    def __init__(self, board, analyst):
+        self._board = board
+        self._analyst = analyst
+
+    def display_board(self):
+        for i in range(self._board.SIZE):
+            print(ascii_uppercase[i] + "\u2502" + ' '.join([str(x.value) for x in self._board.row(i)]))
+        print(" \u2514" + "\u2500" * (self._board.SIZE * 2 - 1))
+        print("  " + ' '.join(str(x + 1) for x in range(self._board.SIZE)))
