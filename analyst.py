73f3a28960443530d3248c35f266e840e3f871db
jreyesr <jreyesr@users.noreply.github.com>
2016-11-18 16:00:07 -0500

    Massive documentation added

--- analyst.py
+++ analyst.py
@@ -3,9 +3,19 @@ from board import Board, Color
 
 class BoardAnalyst:
     def __init__(self, board_to_watch):
+        """
+        Create an Analyst that watches and reports status of a Board
+        :param board_to_watch: A Board that will be binded to the Analyst
+        """
         self.board = board_to_watch
 
     def neighs(self, i, j):
+        """
+        Return all neighbours of place (i,j): two if in corner, three if in side, four if inside board
+        :param i: The row of the place to analyze
+        :param j: The column of the place to analyze
+        :return: A list containing tuples of ints, so that 2<=len(X)<=4
+        """
         vert_neighs = []
         if i > 0:
             vert_neighs.append((i - 1, j))
@@ -21,19 +31,41 @@ class BoardAnalyst:
         return vert_neighs + horz_neighs
 
     def friend_neighs(self, i, j):
+        """
+        Return all neighbours of (i,j) that have THE SAME COLOR as it, and nothing if (i,j) is blank or has no friends
+        :param i: The row of the place to analyze
+        :param j: The column of the place to analyze
+        :return: A list containing tuples of ints that are to a side of (i,j) and have the same color as it
+        """
         if self.board.item(i, j) == Color.Blank:
             return []
         return [x for x in self.neighs(i, j) if self.board.item(x[0], x[1]) == self.board.item(i, j)]
 
     def has_friends(self, i, j):
+        """
+        Return True if (i,j) has any neighbour with the same color as it and (i,j) is not blank, False otherwise
+        """
         if self.board.item(i, j) == Color.Blank:
             return False
         return len(self.friend_neighs(i, j)) != 0
 
     def all_friends(self, i, j):
+        """
+        Return (i,j) plus every item that has the same color as (i,j) and is DIRECTLY connected to it
+        'A is directly connected to B' means 'A is connected to B via items of the same color as A *and* B'
+        :param i: The row of the place to analyze
+        :param j: The column of the place to analyze
+        :return: A list of items of the same color and neighbours of each other
+        """
         return self._analyze([], [(i, j)])
 
     def _analyze(self, already_seen, to_check):
+        """
+        Find all non-seen friend neighbours of everyone in to_check
+        :param already_seen: A list containing positions to be ignored
+        :param to_check: A list containing the items to be checked for friend neighbours
+        :return: already_seen, plus to_check, plus the friend neighbors of everyone in to_check, ad so on...
+        """
         if not to_check: return already_seen
         updated_seen = already_seen + to_check
         new_to_check = []
@@ -48,6 +80,9 @@ class BoardAnalyst:
         return 10
 
     def any_friends(self):
+        """
+        Return True if any item in the board has friends, False otherwise
+        """
         for i in range(Board.SIZE):
             for j in range(Board.SIZE):
                 if self.has_friends(i, j):
--- board.py
+++ board.py
@@ -5,48 +5,94 @@ class Board:
     SIZE = 10
 
     def __init__(self):
-        self.fill_with(Color.Blank)
+        """
+        Initialize a blank Board, of size Board.SIZE x Board.SIZE
+        """
+        self._contents = [[Color.Blank for _ in range(Board.SIZE)] for _ in range(Board.SIZE)]
 
     def __str__(self):
+        """
+        Return a string representation of the Board
+        """
         rows = []
         for i in range(Board.SIZE):
             rows.append("|" + " ".join([str(c.value) for c in self.row(i)]) + "|")
         return '\n'.join(rows)
 
     def random_fill(self):
+        """
+        Randomly fill the board with colors, except Blank
+        """
         from random import choice
         for i in range(Board.SIZE):
             for j in range(Board.SIZE):
                 self.set_item(i, j, choice([element for element in Color if element != Color.Blank]))
 
     def fill_with(self, color):
+        """
+        Fill the board with the designated color
+        :param color: The Color to fill the board with
+        """
         self._contents = [[color for _ in range(Board.SIZE)] for _ in range(Board.SIZE)]
 
     def row(self, i):
+        """
+        Return a list containing row i of the board
+        """
         return self._contents[i]
 
     def column(self, j):
+        """
+        Return a list containing column j of the board
+        """
         return [row[j] for row in self._contents]
 
     def item(self, i, j):
+        """
+        Return item at position (i,j) of the board
+        """
         return self._contents[i][j]
 
     def set_row(self, i, new_row):
+        """
+        Set row i of the board with the values in new_row
+        :param i: The index of the row to be replaced
+        :param new_row: A list of new values. it is assumed that len(new_values)>=Board.SIZE
+        """
         self._contents[i] = new_row
 
     def set_column(self, j, new_column):
+        """
+        Set column j of the board with the values in new_row
+        :param i: The index of the row to be replaced
+        :param new_row: A list of new values. it is assumed that len(new_values)>=Board.SIZE
+        """
         for i in range(Board.SIZE):
             self._contents[i][j] = new_column[i]
 
     def set_item(self, i, j, new_item):
+        """
+        Set item (i,j) of the board with the color on new_item
+        :param i: The row of the item to be replaced
+        :param j: The column of the item to be replaced
+        :param new_item: The new value to be stored
+        """
         self._contents[i][j] = new_item
 
     def column_compacted(self, j):
+        """
+        Return a copy of column j, with all Blank items at the top
+        :param j: The index of the column to analyze
+        """
         not_zeroes = [x for x in self.column(j) if x != Color.Blank]
         zeroes = [Color.Blank for _ in range(Board.SIZE - len(not_zeroes))]
         return zeroes + not_zeroes
 
     def compact_column(self, j):
+        """
+        Set column j to be its compacted version
+        :param j: The index of the column to modify
+        """
         self.set_column(j, self.column_compacted(j))
 
     def compact_all(self):
@@ -54,6 +100,10 @@ class Board:
         pass
 
     def clear_items(self, list_to_clear):
+        """
+        Set every item in list_to_clear to be Blank
+        :param list_to_clear: A list of items to erase
+        """
         for i, j in list_to_clear:
             self.set_item(i, j, Color.Blank)
 
