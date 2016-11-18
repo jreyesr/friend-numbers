93e4720668adb22b6654702a3ba9cab98533214c
jreyesr <jreyesr@users.noreply.github.com>
2016-11-18 15:36:17 -0500

    MAJOR EDIT gui_displayer Full edit. Now contains a class for every different window (main menu, About, name enter, game, Game Over) with the appropriate transitions between them.
    main
    Nothing of importance

--- gui_displayer.py
+++ gui_displayer.py
@@ -1,69 +1,130 @@
 from string import ascii_uppercase
 from tkinter import *
 
-from board import Color
+from analyst import BoardAnalyst
+from board import Board, Color
 
 
-class GuiDisplayer():
-    _score_label = None
-    _score = 0
-    _buttons = None
-    current_window = None
+class MainMenuWindow:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("+100+100")
+        Grid.columnconfigure(self.root, 0, weight=1)
+        Grid.rowconfigure(self.root, 0, weight=1)
 
-    def __init__(self, board, analyst):
-        self._board = board
-        self._analyst = analyst
-        self._buttons = [[0 for _ in range(self._board.SIZE + 1)] for _ in range(self._board.SIZE + 1)]
+        frame = Frame(self.root, borderwidth=10)
+        frame.grid(row=0, column=0, sticky=N + S + E + W)
 
-    def button_clicked(self, i, j):
-        if not self._analyst.has_friends(i, j) or self._board.item(i, j) == Color.Blank:
-            return
-        to_clear = self._analyst.all_friends(i, j)
-        self._score += self._analyst.score(to_clear)
-        self._board.clear_items(to_clear)
-        self._board.compact_all()
-        self._score_label.configure(text="Puntos: {}".format(self._score))
+        Button(frame, text="New Game", command=lambda: self.start_new_game()).grid(row=0, column=0, sticky=E + W)
+        Button(frame, text="About", command=lambda: self.show_about()).grid(row=1, column=0, sticky=E + W)
+        Button(frame, text="Exit", command=lambda: self.exit()).grid(row=2, column=0, sticky=E + W)
+        Grid.columnconfigure(frame, 0, weight=1)
+        for i in range(3):
+            Grid.rowconfigure(frame, i, weight=1)
 
-        if not self._analyst.any_friends():
-            self.end_game()
+    def show(self):
+        self.root.mainloop()
 
-        self.update()
+    def start_new_game(self):
+        new_window = NameWindow()
+        self.root.destroy()
+        new_window.show()
 
-    @staticmethod
-    def open(window):
-        GuiDisplayer.current_window = window
-        window.mainloop()
+    def show_about(self):
+        new_window = AboutWindow()
+        self.root.destroy()
+        new_window.show()
 
-    @staticmethod
-    def close():
-        GuiDisplayer.current_window.destroy()
-        GuiDisplayer.current_window = None
+    def exit(self):
+        self.root.destroy()
 
-    def main_window(self):
-        root = Tk()
-        Grid.rowconfigure(root, 0, weight=1)
 
-        upper_frame = Frame(root, borderwidth=10)
+class NameWindow:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("+100+100")
+        Grid.columnconfigure(self.root, 0, weight=1)
+        Grid.rowconfigure(self.root, 0, weight=1)
+
+        self.text_field = Entry(self.root, justify=CENTER)
+        self.text_field.grid(row=0, column=0, sticky=E + W, padx=5, pady=5)
+        Button(self.root, text="OK", command=lambda: self.start_game()).grid(row=1, column=0, padx=5, pady=5)
+
+    def show(self):
+        self.root.mainloop()
+
+    def start_game(self):
+        board = Board()
+        board.random_fill()
+        analyst = BoardAnalyst(board)
+
+        new_window = GameWindow(self.text_field.get(), board, analyst)
+        self.root.destroy()
+        new_window.show()
+
+
+class AboutWindow:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("300x200+100+100")
+        Grid.rowconfigure(self.root, 0, weight=1)
+        Grid.columnconfigure(self.root, 0, weight=1)
+
+        frame = Frame(self.root, borderwidth=10)
+        text = Text(frame)
+        text.pack(fill=BOTH, expand=1)
+        text.insert(END, "About the game\nDesigned for ESPOL (...)")
+        frame.grid(row=0, column=0, sticky=N + S + E + W)
+
+        second_frame = Frame(self.root, borderwidth=10)
+        second_frame.grid(row=1, column=0)
+        Button(second_frame, text="OK", command=lambda: self.close()).grid(row=0, column=0)
+
+    def show(self):
+        self.root.mainloop()
+
+    def close(self):
+        new_window = MainMenuWindow()
+        self.root.destroy()
+        new_window.show()
+
+
+class GameWindow:
+    def __init__(self, player_name, board, analyst):
+        self.player_name = player_name
+        self.score = 0
+
+        self.board = board
+        self.analyst = analyst
+        self.buttons = [[0 for _ in range(self.board.SIZE + 1)] for _ in range(self.board.SIZE + 1)]
+
+        self.root = Tk()
+        self.root.geometry("500x500+100+100")
+        Grid.rowconfigure(self.root, 0, weight=1)
+        Grid.columnconfigure(self.root, 0, weight=1)
+
+        upper_frame = Frame(self.root, borderwidth=10)
         upper_frame.grid(row=0, column=0, sticky=N + S + E + W)
 
-        for row_index in range(self._board.SIZE):
+        for row_index in range(self.board.SIZE):
             Grid.rowconfigure(upper_frame, row_index, weight=1)
-            for col_index in range(self._board.SIZE):
+            for col_index in range(self.board.SIZE):
                 Grid.columnconfigure(upper_frame, col_index + 1, weight=1)
                 btn = Button(upper_frame, command=lambda x=row_index, y=col_index: self.button_clicked(x, y))
                 btn.configure(bg=self.get_color(row_index, col_index))
-                self._buttons[row_index][col_index] = btn
+                self.buttons[row_index][col_index] = btn
                 btn.grid(row=row_index, column=col_index + 1, sticky=N + S + E + W, padx=2, pady=2)
 
         # Set labels
-        for i in range(self._board.SIZE):
+        for i in range(self.board.SIZE):
             Label(upper_frame, text=ascii_uppercase[i]).grid(row=i, column=0, sticky=N + S + E + W)
-        for j in range(self._board.SIZE):
-            Label(upper_frame, text=str(j + 1)).grid(row=self._board.SIZE, column=j + 1, sticky=N + S + E + W)
+        for j in range(self.board.SIZE):
+            Label(upper_frame, text=str(j + 1)).grid(row=self.board.SIZE, column=j + 1, sticky=N + S + E + W)
 
         # Set additional info (score, exit button)
-        lower_frame = Frame(root)
+        lower_frame = Frame(self.root)
         lower_frame.grid(row=1, column=0, sticky=N + S + E + W)
+
         Grid.rowconfigure(lower_frame, 0, weight=1)
         Grid.columnconfigure(lower_frame, 0, weight=1)
         Grid.columnconfigure(lower_frame, 1, weight=1)
@@ -73,31 +134,78 @@ class GuiDisplayer():
         self._score_label = lbl
         lbl.grid(row=0, column=0, sticky=N + S + E + W, padx=5, pady=5)
         # Exit game button
-        Button(lower_frame, text="Exit", command=lambda: self.end_game()).grid(row=0, column=1,
-                                                                               sticky=N + S + E + W, padx=20,
-                                                                               pady=5)
-        return root
+        Button(lower_frame, text="End game", command=lambda: self.end_game()).grid(row=0, column=1,
+                                                                                   sticky=N + S + E + W, padx=20,
+                                                                                   pady=5)
 
-    def update(self):
-        for i in range(self._board.SIZE):
-            for j in range(self._board.SIZE):
-                try:
-                    self._buttons[i][j].configure(bg=self.get_color(i, j))
-                except:
-                    pass
+    def show(self):
+        self.root.mainloop()
 
     def end_game(self):
-        print("Game has ended now. Score: {}".format(self._score))
-        self.close()
+        new_window = GameOverWindow(self.player_name, self.score)
+        self.root.destroy()
+        new_window.show()
+
+    def button_clicked(self, i, j):
+        if not self.analyst.has_friends(i, j) or self.board.item(i, j) == Color.Blank:
+            return
+        to_clear = self.analyst.all_friends(i, j)
+        self.score += self.analyst.score(to_clear)
+        self.board.clear_items(to_clear)
+        self.board.compact_all()
+        self._score_label.configure(text="Puntos: {}".format(self.score))
+
+        if not self.analyst.any_friends():
+            self.end_game()
+
+        self.update()
+
+    def update(self):
+        for i in range(self.board.SIZE):
+            for j in range(self.board.SIZE):
+                try:
+                    self.buttons[i][j].configure(bg=self.get_color(i, j))
+                except TclError:
+                    pass
 
     def get_color(self, i, j):
-        if self._board.item(i, j) == Color.A:
+        if self.board.item(i, j) == Color.A:
             return 'red'
-        elif self._board.item(i, j) == Color.B:
+        elif self.board.item(i, j) == Color.B:
             return 'green'
-        elif self._board.item(i, j) == Color.C:
+        elif self.board.item(i, j) == Color.C:
             return 'blue'
-        elif self._board.item(i, j) == Color.D:
+        elif self.board.item(i, j) == Color.D:
             return 'yellow'
         else:
             return 'gray'
+
+
+class GameOverWindow:
+    def __init__(self, player_name, score):
+        self.player_name = player_name
+        self.score = score
+
+        self.root = Tk()
+        self.root.geometry("+100+100")
+        Grid.columnconfigure(self.root, 0, weight=1)
+        Grid.rowconfigure(self.root, 0, weight=1)
+
+        frame = Frame(self.root, borderwidth=10)
+        frame.grid(row=0, column=0, sticky=N + S + E + W)
+
+        Label(frame, text=player_name).grid(row=0, column=0)
+        Label(frame, text="{} puntos".format(score)).grid(row=1, column=0)
+        Button(frame, text="OK", command=lambda: self.close()).grid(row=2, column=0)
+
+        Grid.columnconfigure(frame, 0, weight=1)
+        Grid.rowconfigure(frame, 0, weight=1)
+        Grid.rowconfigure(frame, 1, weight=1)
+
+    def close(self):
+        new_window = MainMenuWindow()
+        self.root.destroy()
+        new_window.show()
+
+    def show(self):
+        self.root.mainloop()
--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-# graph = {'A': [], 'B': [], 'C': [], 'D': [], 'E': ['F'], 'F': ['B', 'E', 'G', 'J'], 'G': ['F', 'K'], 'H': [], 'I': [],
-#          'J': ['F', 'K', 'N'], 'K': ['G', 'J','O'], 'L': [], 'M': [], 'N': ['J', 'O'], 'O': ['K', 'N', 'P'],
-#          'P': ['O']}
-#
-#
-# def find_all_friends(x):
-#     global already_seen
-#     already_seen = already_seen if already_seen != [] else [x]
-#     new_neighs = [friend for friend in graph[x] if friend not in already_seen]
-#     already_seen += new_neighs
-#     for neigh in new_neighs:
-#         find_all_friends(neigh)
-#
-#
-# already_seen = []
-# find_all_friends('E')
-# print(already_seen)
+from gui_displayer import *
 
-from analyst import BoardAnalyst
-from board import Board
-from console_displayer import ConsoleDisplayer
-from gui_displayer import GuiDisplayer
-
-b = Board()
-b.random_fill()
-
-a = BoardAnalyst(b)
-d = ConsoleDisplayer(b, a)
-g = GuiDisplayer(b, a)
-
-d.display()
-root = g.main_window()
-g.open(root)
-g.update()
+main_menu = MainMenuWindow()
+main_menu.show()
